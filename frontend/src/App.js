58be5a9a1a3f25de3ffc14366d72b731bd4cf4e6
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2022-12-23 16:29:03 +0530

    Adding feature product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,36 @@
+import { Link } from 'react-router-dom';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href='/'>Amazon</a>
+        <Link to="/">
+          <img className="brand" src="/images/logo_white.png" alt="amazon" />
+        </Link>
       </header>
       <main>
-        List product 
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {data.products.map((product, i) => (
+            <div className="product" key={i}>
+              <Link to={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </Link>
+              <div className="product_info">
+                <Link style={{textDecoration: 'none', color: 'black'}} to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
+                <p>
+                  <strong>₹{product.price}</strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike_slim_shirt',
+            catagory: 'Shirt',
+            image: '/images/p1.jpeg',
+            price: 500,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            reviews: 10,
+            description: 'This a high quality product'
+        },
+
+        {
+            name: 'Zara Jeans',
+            slug: 'zara_jeans',
+            catagory: 'Jeans',
+            image: '/images/p2.jpeg',
+            price: 500,
+            countInStock: 10,
+            brand: 'Zara',
+            rating: 4,
+            reviews: 10,
+            description: 'This a high quality product'
+        },
+
+        {
+            name: 'Gucci Top',
+            slug: 'gucci_top',
+            catagory: 'Top',
+            image: '/images/p3.jpeg',
+            price: 500,
+            countInStock: 10,
+            brand: 'Gucci',
+            rating: 4.5,
+            reviews: 10,
+            description: 'This a high quality product'
+        },
+
+        {
+            name: 'Nike Shoes',
+            slug: 'nike_shoes',
+            catagory: 'Shirt',
+            image: '/images/p4.jpeg',
+            price: 500,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 5,
+            reviews: 10,
+            description: 'This a high quality product'
+        },
+    ]
+}
+
+
+export default data
