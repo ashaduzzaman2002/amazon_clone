8faaacc3038a061c578193b180d7b7a344f2e374
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2022-12-23 15:31:38 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Amazon</a>
+      </header>
+      <main>
+        List product 
+      </main>
+    </div>
+  );
+}
+
+export default App;
